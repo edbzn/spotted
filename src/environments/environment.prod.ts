b4f0e830a61e27fb6e05be95bc2fbb7007693d91
edouardbozon <bozonedouard@gmail.com>
2018-06-12 19:22:55 +0200

    Fix firebase collection add and list view

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,26 +4,24 @@ import { Environment } from './model';
 
 export const environment: Environment = {
   production: true,
-  showDevModule: false,
+  showDevModule: false
   /**
    * Angular debug tools in the dev console
    * https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
    * @param modRef
    * @return {any}
-   */
+   */,
   decorateModuleRef(modRef: NgModuleRef<any>): any {
     disableDebugTools();
     return modRef;
   },
-
   ENV_PROVIDERS: [],
-
   firebase: {
     apiKey: 'AIzaSyBuBD4A_IkayjEIqwjUO_6ewKC38eKAX4k',
     authDomain: 'spotted-1528021262358.firebaseapp.com',
     databaseURL: 'https://spotted-1528021262358.firebaseio.com',
     projectId: 'spotted-1528021262358',
-    storageBucket: '',
+    storageBucket: 'spotted-1528021262358.appspot.com',
     messagingSenderId: '530601209274',
   },
 };
