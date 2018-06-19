70a8e66cb737071034222de997f9eff9581f459b
edouardbozon <bozonedouard@gmail.com>
2018-06-19 20:27:57 +0200

    Provide translation in lazy-loaded authentication module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import {
@@ -8,54 +8,20 @@ import {
   TranslateLoader,
   TranslateService,
 } from '@ngx-translate/core';
-import { HttpLoaderFactory } from '../app.translate.factory';
-import { StorageService } from '../core/storage.service';
 
-const sharedModules = [
+export const sharedModules = [
   CommonModule,
   FormsModule,
   ReactiveFormsModule,
   HttpClientModule,
   MaterialModule,
-  TranslateModule,
-];
-
-const providers = [
-  {
-    provide: TranslateLoader,
-    useFactory: HttpLoaderFactory,
-    deps: [HttpClient],
-  },
-  TranslateService,
 ];
 
 @NgModule({
   imports: [...sharedModules],
-  providers: [...providers],
+  providers: [],
   declarations: [],
   entryComponents: [],
   exports: [...sharedModules],
 })
-export class SharedModule {
-  constructor(translateService: TranslateService, storage: StorageService) {
-    const defaultLang =
-      storage.get('defaultLang') || translateService.getBrowserLang();
-
-    translateService.setDefaultLang(defaultLang);
-    translateService.use(defaultLang);
-  }
-
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SharedModule,
-      providers: [
-        {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-        TranslateService,
-      ],
-    };
-  }
-}
+export class SharedModule {}
