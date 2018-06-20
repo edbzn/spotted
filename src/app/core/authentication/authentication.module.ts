a03a13e6e4c7aafdf04f7f3473ccdf78e99c9177
edouardbozon <bozon.edouard@live.fr>
2018-06-20 16:02:40 +0200

    Rename user component in profile component

--- src/app/core/authentication/authentication.module.ts
+++ src/app/core/authentication/authentication.module.ts
@@ -2,7 +2,7 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpLoaderFactory } from '../../app.translate.factory';
@@ -21,7 +21,7 @@ import { HttpClient } from '@angular/common/http';
     }),
     AuthenticationRoutingModule,
   ],
-  declarations: [LoginComponent, RegisterComponent, UserComponent],
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
   bootstrap: [LoginComponent],
 })
 export class AuthenticationModule {}
