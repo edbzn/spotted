f7edbe44d1043cf5bacd03422af4a8145d5beee1
edouardbozon <bozonedouard@gmail.com>
2018-06-20 20:41:49 +0200

    Fix auth guard spec

--- src/app/core/authentication/auth-guard.service.spec.ts
+++ src/app/core/authentication/auth-guard.service.spec.ts
@@ -1,15 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { AuthGuardService } from './auth-guard.service';
+import { AuthGuard } from './auth-guard.service';
 
 describe('AuthGuardService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthGuardService]
-    });
+    TestBed.configureTestingModule({ providers: [AuthGuard] });
   });
 
-  it('should be created', inject([AuthGuardService], (service: AuthGuardService) => {
+  it('should be created', inject([AuthGuard], (service: AuthGuard) => {
     expect(service).toBeTruthy();
   }));
 });
