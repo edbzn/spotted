47392208491ce7b8e312caf02fc5f5dd5e5aa1cc
edouardbozon <bozon.edouard@live.fr>
2018-06-25 10:32:06 +0200

    Add fade animation on detail routing

--- src/app/core/modules/spot/detail/detail.component.ts
+++ src/app/core/modules/spot/detail/detail.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from '../../../../shared/router-animation';
 
 @Component({
   selector: 'spt-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
+  animations: [fadeAnimation],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: { '[@fadeAnimation]': '' },
 })
 export class DetailComponent implements OnInit {
   constructor() {}
