b3937a354ee93a93aaa74e55f2c43e73a494dca2
Koala <bozonedouard@gmail.com>
2018-09-24 14:25:11 +0200

    Fix logout action

--- src/app/authentication/profile/profile.component.ts
+++ src/app/authentication/profile/profile.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
-  Validators,
-  FormGroup,
   FormBuilder,
   FormControl,
+  FormGroup,
+  Validators,
 } from '@angular/forms';
-import { PasswordValidation } from '../../shared/match-password.validator';
-import { appConfiguration } from '../../app-config';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { MatSnackBar } from '@angular/material';
 import { TranslateService } from '@ngx-translate/core';
-import { from } from 'rxjs';
-import { mergeMap, mergeMapTo, take, filter } from 'rxjs/internal/operators';
+
+import { appConfiguration } from '../../app-config';
 import { fade } from '../../shared/animations';
-import { Router } from '@angular/router';
+import { PasswordValidation } from '../../shared/match-password.validator';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'spt-profile',
@@ -23,7 +21,7 @@ import { Router } from '@angular/router';
   // tslint:disable-next-line:use-host-property-decorator
   host: { '[@fade]': '' },
 })
-export class ProfileComponent implements OnInit, OnDestroy {
+export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
   passwordForm: FormGroup;
 
@@ -44,105 +42,78 @@ export class ProfileComponent implements OnInit, OnDestroy {
   }
 
   constructor(
-    public auth: AngularFireAuth,
+    public auth: AuthService,
     private fb: FormBuilder,
     private snackBar: MatSnackBar,
-    private translate: TranslateService,
-    private router: Router
+    private translate: TranslateService
   ) {}
 
   ngOnInit(): void {
-    this.auth.user.subscribe(user => {
-      if (user) {
-        this.profileForm = this.fb.group({
-          name: [
-            user.displayName,
-            [
-              Validators.required,
-              Validators.minLength(appConfiguration.forms.nameMinLength),
-            ],
+    const { user } = this.auth;
+
+    this.profileForm = this.fb.group({
+      name: [
+        user.displayName,
+        [
+          Validators.required,
+          Validators.minLength(appConfiguration.forms.nameMinLength),
+        ],
+      ],
+      email: [user.email, [Validators.required, Validators.email]],
+    });
+
+    this.passwordForm = this.fb.group(
+      {
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(appConfiguration.forms.passwordMinLength),
           ],
-          email: [user.email, [Validators.required, Validators.email]],
-        });
-
-        this.passwordForm = this.fb.group(
-          {
-            password: [
-              '',
-              [
-                Validators.required,
-                Validators.minLength(appConfiguration.forms.passwordMinLength),
-              ],
-            ],
-            confirmPassword: [
-              '',
-              [
-                Validators.required,
-                Validators.minLength(appConfiguration.forms.passwordMinLength),
-              ],
-            ],
-          },
-          { validator: PasswordValidation.MatchPassword }
-        );
-      }
-    });
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(appConfiguration.forms.passwordMinLength),
+          ],
+        ],
+      },
+      { validator: PasswordValidation.MatchPassword }
+    );
   }
 
-  ngOnDestroy(): void {}
-
-  logout(): void {
-    this.auth.auth.signOut().then(() => {
-      this.router.navigateByUrl('/');
-    });
-  }
-
-  updateProfile(): void {
+  async updateProfile(): Promise<void> {
     if (!this.profileForm.valid) {
       return;
     }
 
-    this.auth.user
-      .pipe(
-        filter(user => !!user),
-        mergeMap(user => from(user.updateEmail(this.email.value as string))),
-        mergeMapTo(this.auth.user),
-        take(1),
-        mergeMap(user =>
-          from(
-            user.updateProfile({
-              photoURL: user.photoURL,
-              displayName: this.name.value,
-            })
-          )
-        )
-      )
-      .subscribe(_ => {
-        this.translate
-          .get(['user.profileChangedSuccess'])
-          .subscribe(texts =>
-            this.snackBar.open(texts['user.profileChangedSuccess'], 'OK')
-          );
-      });
+    const { user } = this.auth;
+    await user.updateEmail(this.email.value);
+    await user.updateProfile({
+      photoURL: user.photoURL,
+      displayName: this.name.value,
+    });
+
+    this.translate
+      .get(['user.profileChangedSuccess'])
+      .subscribe(texts =>
+        this.snackBar.open(texts['user.profileChangedSuccess'], 'OK')
+      );
   }
 
-  updatePassword(): void {
+  async updatePassword(): Promise<void> {
     if (!this.passwordForm.valid) {
       return;
     }
 
-    this.auth.user
-      .pipe(
-        filter(user => !!user),
-        mergeMap(user =>
-          from(user.updatePassword(this.password.value as string))
-        )
-      )
-      .subscribe(_ => {
-        this.translate
-          .get(['user.passwordChangedSuccess'])
-          .subscribe(texts =>
-            this.snackBar.open(texts['user.passwordChangedSuccess'], 'OK')
-          );
-      });
+    const { user } = this.auth;
+    await user.updatePassword(this.password.value);
+
+    this.translate
+      .get(['user.passwordChangedSuccess'])
+      .subscribe(texts =>
+        this.snackBar.open(texts['user.passwordChangedSuccess'], 'OK')
+      );
   }
 }
